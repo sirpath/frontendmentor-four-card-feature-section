76214f1be88c1c4edab19d9acfcc6f2bfd1c1cf0
sirpath <surapath.sk@gmail.com>
2023-08-25 15:51:34 +0700

    finish four card frontendmentor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        
+      },
+      colors: {
+        primary: "hsl(234, 12%, 34%)",
+        secondary: "hsl(229, 6%, 66%)",
+        rear: "hsl(0, 0%, 98%)",
+        boxred: "hsl(0, 78%, 62%)",
+        boxcyan: "hsl(180, 62%, 55%)",
+        boxorange: "hsl(34, 97%, 64%)",
+        boxblue: "hsl(212, 86%, 64%)",
+      }
+    },
   },
   plugins: [],
 }
